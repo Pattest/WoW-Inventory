e8f50a82365c4c19dfc90edd13228d5ed197610f
Pattest <baptistecadoux@gmail.com>
2023-10-16 16:04:01 +0200

    first commit

---
+++ WoW-Inventory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WoW-Inventory
+//
+//  Created by Baptiste Cadoux on 16/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
